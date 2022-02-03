7aa54d791189435f50463a8c69d8dfdd0035d8ea
Birkan <alibirkan9813@gmail.com>
2022-02-03 15:26:16 +0300

    motor driver drv8825 changed

--- final_project_v1.0.ino
+++ final_project_v1.0.ino
@@ -1,14 +1,11 @@
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━ Library ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-#include <Stepper.h> // Stepper motor Library
 #include <FirebaseESP32.h> // Firebase Library
 #include <WiFiManager.h> // Wifi Manager Library
-
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━ Definitions ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-const int motorStep = 200; // Motor step
-const int laps = 21.5; // Motor number of laps
-const int motorRPM = 240; // Motorun rpm (step/second)
-//Stepper stepperMotor(motorStep, D8,D7,D6,D5); // Step motor definition (nodemcu)
-Stepper stepperMotor(motorStep, 15,21,4,5); // Step motor definition (esp32)
+const int dirPin = 2;
+const int stepPin = 3;
+const int stepsPerRevolution = 200;
+const int stepMotorDelay=50;
 const int relay=13 ; // Motor relay
 const String machineID=""; // Machine ID
 const int backSwitch=34; // Back limit switch pin
@@ -18,9 +15,7 @@ const int frontIR=39; // Front IR sensor
 int trigPin1=32,trigPin2=25,trigPin3=27;
 int echoPin1=33,echoPin2=26,echoPin3=14;
 double v=0; // Volume
-
-bool coverStatus=0;
-
+bool coverStatus=false;
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━ Firebase Definitions ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 // Database is firebase
 #define DATABASE_URL "url"
@@ -31,12 +26,18 @@ FirebaseConfig config;
 String path = "path"; // Database path (Database için gerekli yol)
 bool res;
 WiFiManager wm; // Wifi manager object
-
+void motorWork(){
+  for(int x = 0; x < stepsPerRevolution; x++){
+		digitalWrite(stepPin, HIGH); delayMicroseconds(stepMotorDelay);
+		digitalWrite(stepPin, LOW); delayMicroseconds(stepMotorDelay);
+	}
+}
 void goBack(){ // Go to back
   digitalWrite(role,LOW); // Relay on
   delayMicroseconds(5);
   while(!digitalRead(backSwitch)){
-    stepperMotor.step(-(motorStep)); // The motor step is multiplied by the number of revolutions and it goes back that many steps.
+    digitalWrite(dirPin, LOW);
+    motorWork(); // The motor step is multiplied by the number of revolutions and it goes back that many steps.
     // delay(1); // Delay function (1-10ms)
   }
   delayMicroseconds(5);
@@ -47,7 +48,8 @@ void goOn(){ // Go to front
   digitalWrite(role,LOW); // Relay on
   delayMicroseconds(5);
   while(!digitalRead(frontSwitch)){
-    stepperMotor.step(motorStep); // The motor step is multiplied by the number of revolutions and it goes back that many steps.
+    digitalWrite(dirPin, HIGH);
+    motorWork();// The motor step is multiplied by the number of revolutions and it goes back that many steps.
     // delay(1); // Delay function (1-10ms)
   }
   delayMicroseconds(5);
@@ -57,19 +59,21 @@ void goOn(){ // Go to front
 void findLocation(){ // Location finding
   if(!digitalRead(frontSwitch))
     goBack();
-  coverStatus=0;
+  coverStatus=false;
 }
-
 void isObject(){
   if (!digitalRead(backSwitch))
-    goBack();
+    findLocation();
   if(digitalRead(backIR)){
     digitalWrite(role,LOW); // Relay on
     delayMicroseconds(5);
-    while(!digitalRead(frontIR))
-      stepperMotor.step(motorStep);
+    digitalWrite(dirPin, LOW);
+    while(!digitalRead(frontIR))      
+      motorWork();
     while (digitalRead(frontSwitch)){
-      stepperMotor.step(motorStep/20); // The motor step is multiplied by the number of revolutions and it goes back that many steps.
+      stepsPerRevolution=100;
+      motorWork(); // The motor step is multiplied by the number of revolutions and it goes back that many steps.
+      stepsPerRevolution=200;
       v+=volumeMeasurement()*0.1*; // Sensibility 0.01 and cm->mm == 0.1
     }
     Serial.print("Volume measurement : ");
@@ -77,7 +81,6 @@ void isObject(){
     goBack();
   }
 }
-
 void distanceMeasurement(int trigPin,int echoPin){
   digitalWrite(trigPin, LOW);
   delayMicroseconds(5);
@@ -88,7 +91,6 @@ void distanceMeasurement(int trigPin,int echoPin){
   distance= time /29.1/2;
   returun distance;
 }
-
 void volumeMeasurement(){ // Volume measurement
   y1=distanceMeasurement(trigPin1,echoPin1);
   y2=distanceMeasurement(trigPin2,echoPin2);
@@ -98,14 +100,11 @@ void volumeMeasurement(){ // Volume measurement
     h=15-h;
   return x*h;
 }
-
 void setup(){ // Setup function
   Serial.begin(115200); // Serial screen buand
-  stepperMotor.setSpeed(motorRPM); // Step motor speed definition
   // wm.resetSettings(); // Wifi data clear
   WiFi.mode(WIFI_STA);  // Wifi mode
   res=wm.autoConnect("Volume","12345678"); // Wifi name and password
-
  if(!res)
    Serial.println("Wifi connection failed!");
  else
@@ -120,12 +119,12 @@ void setup(){ // Setup function
   config.signer.tokens.legacy_token = DATABASE_SECRET;
   Firebase.reconnectWiFi(true);
   Firebase.begin(&config, &auth);
+  pinMode(stepPin, OUTPUT); // Step motor definition
+	pinMode(dirPin, OUTPUT); // Step motor definition
   pinMode(backSwitch,INPUT); pinMode(frontSwitch,INPUT);
   pinMode(backIR,INPUT); pinMode(frontIR,INPUT);
-
   findLocation(); // Finding Location
 }
-
 void loop(){ // Loop function
   isObject();
 }

f39b37265b65ad7e03446f80ea9dabda44d78866
Birkan <alibirkan9813@gmail.com>
2022-06-14 16:11:39 +0300

    • Update

--- final_project_v1.0.1/main_code.py
+++ final_project_v1.0.1/main_code.py
@@ -1,4 +1,5 @@
-import time,RPi.GPIO as GPIO
+# ━━━━━━━━━━━━━━━━━━━━━ Library ━━━━━━━━━━━━━━━━━━━━━
+import time,pyrebase,RPi.GPIO as GPIO
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
@@ -12,9 +13,27 @@ config = {
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
 
-company,path,machineID,errorcode = "",db,"7539",200 # Machine ID definitions
+# ━━━━━━━━━━━━━━━━━━━━━ Variable definition ━━━━━━━━━━━━━━━━━━━━━
+company,path,machineID,errorcode = "",db,"machineID","200" # Machine ID definitions
 hourNow,tempHour= datetime.now().hour, datetime.now().hour # Now time
 TRIG_Pin,ECHO_Pin=6,5 # Pin definition
+
+# ━━━━━━━━━━━━━━━━━━━━━ Distance function ━━━━━━━━━━━━━━━━━━━━━
+def distance(): # Distance
+    #global path
+    GPIO.output(TRIG_Pin, False)
+    time.sleep(2)
+    GPIO.output(TRIG_Pin, True)
+    time.sleep(0.00001)
+    GPIO.output(TRIG_Pin, False)
+    while GPIO.input(ECHO_Pin)==0:
+        pulse_start = time.time()
+    while GPIO.input(ECHO_Pin)==1:
+        pulse_end = time.time()
+    pulse_duration = pulse_end-pulse_start
+    distance = pulse_duration*17150
+    distance = round(distance,2)
+    return distance
 
 # ━━━━━━━━━━━━━━━━━━━━━ Setup function ━━━━━━━━━━━━━━━━━━━━━
 def setup(): #Setup
@@ -29,13 +48,15 @@ def setup(): #Setup
     GPIO.setup(TRIG_Pin, GPIO.OUT)
     print("Setup ok")
 
-def loop(): # Loop function
+# ━━━━━━━━━━━━━━━━━━━━━ Loop function ━━━━━━━━━━━━━━━━━━━━━
+def loop(): # Loop
     global db,company,path,machineID,errorcode
     global hourNow,tempHour
     global TRIG_Pin,ECHO_Pin
     hourNow,tempHour= datetime.now().hour, datetime.now().hour # Now time
     print(hourNow)
 
+# ━━━━━━━━━━━━━━━━━━━━━ Main Code ━━━━━━━━━━━━━━━━━━━━━
 if __name__ == "__main__": # Main funtion
     setup() # Setup worked
     while True: # Loop started

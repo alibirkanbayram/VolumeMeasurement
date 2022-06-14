23c664a68d1fb6d006630b0ff7c5d5ae28a3659e
Birkan <alibirkan9813@gmail.com>
2022-06-14 16:42:26 +0300

    • Update

---
+++ mobile/volume_measurement/lib/parts/welcome/entrance_part.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EntrancePart extends StatelessWidget {
+  const EntrancePart({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

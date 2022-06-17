0ae4bb72ee9828b561955426d89f6aca02d371f7
Birkan <alibirkan9813@gmail.com>
2022-06-17 08:30:31 +0300

    • Update

--- final_project_v1.0.1/main_code.py
+++ final_project_v1.0.1/main_code.py
@@ -1,129 +1,111 @@
 # ━━━━━━━━━━━━━━━━━━━━━ Library ━━━━━━━━━━━━━━━━━━━━━
-import time,pyrebase,RPi.GPIO as GPIO,numpy as np
-from object_detector import *
+# import time,pyrebase,RPi.GPIO as GPIO,numpy as np
+# from object_detector import *
 
+# ━━━━━━━━━━━━━━━━━━━━━ Desktop Library ━━━━━━━━━━━━━━━━━━━━━
+import cv2
+import utlis
 
 # ━━━━━━━━━━━━━━━━━━━━━ Firebase definition ━━━━━━━━━━━━━━━━━━━━━
-config = {
-  "apiKey": "api_key",
-  "authDomain": "auth_domain",
-  "databaseURL": "database_url",
-  "storageBucket": "storage"
-}
-firebase = pyrebase.initialize_app(config)
-db = firebase.database()
+# config = {
+#   "apiKey": "api_key",
+#   "authDomain": "auth_domain",
+#   "databaseURL": "database_url",
+#   "storageBucket": "storage"
+# }
+# firebase = pyrebase.initialize_app(config)
+# db = firebase.database()
 
 # ━━━━━━━━━━━━━━━━━━━━━ Variable definition ━━━━━━━━━━━━━━━━━━━━━
-company,path,machineID,errorcode = "",db,"machineID","200" # Machine ID definitions
-hourNow,tempHour= datetime.now().hour, datetime.now().hour # Now time
-TRIG_Pin,ECHO_Pin=6,5 # Pin definition
+# company,path,machineID,errorcode = "",db,"machineID","200" # Machine ID definitions
+# hourNow,tempHour= datetime.now().hour, datetime.now().hour # Now time
+# TRIG_Pin,ECHO_Pin=6,5 # Pin definition
 
-# Load Aruco detector
-parameters = cv2.aruco.DetectorParameters_create()
-aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_5X5_50)
+# ━━━━━━━━━━━━━━━━━━━━━ Variable definition (for Desktop) ━━━━━━━━━━━━━━━━━━━━━
+webcam,path,cap,scale =True, 'phone.jpg', cv2.VideoCapture(0), 3
+wP,hP=210 *scale,297 *scale
 
+cap.set(10,160)
+cap.set(3,1920)
+cap.set(4,1080)
 
-# Load Object Detector
-detector = HomogeneousBgDetector()
-
-# Load Cap
-cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
 
 # ━━━━━━━━━━━━━━━━━━━━━ Area Measurement function ━━━━━━━━━━━━━━━━━━━━━
 def areaMeasure(): # Area Measurement
-    _, img = cap.read()
-
-    # Get Aruco marker
-    corners, _, _ = cv2.aruco.detectMarkers(img, aruco_dict, parameters=parameters)
-    if corners:
-
-        # Draw polygon around the marker
-        int_corners = np.int0(corners)
-        cv2.polylines(img, int_corners, True, (0, 255, 0), 5)
-
-        # Aruco Perimeter
-        aruco_perimeter = cv2.arcLength(corners[0], True)
-
-        # Pixel to cm ratio
-        pixel_cm_ratio = aruco_perimeter / 20
-
-        contours = detector.detect_objects(img)
-
-        # Draw objects boundaries
-        for cnt in contours:
-            # Get rect
-            rect = cv2.minAreaRect(cnt)
-            (x, y), (w, h), angle = rect
-
-            # Get Width and Height of the Objects by applying the Ratio pixel to cm
-            object_width = w / pixel_cm_ratio
-            object_height = h / pixel_cm_ratio
-
-            # Display rectangle
-            box = cv2.boxPoints(rect)
-            box = np.int0(box)
-
-            cv2.circle(img, (int(x), int(y)), 5, (0, 0, 255), -1)
-            cv2.polylines(img, [box], True, (255, 0, 0), 2)
-            cv2.putText(img, "Width {} cm".format(round(object_width, 1)), (int(x - 100), int(y - 20)), cv2.FONT_HERSHEY_PLAIN, 2, (100, 200, 0), 2)
-            cv2.putText(img, "Height {} cm".format(round(object_height, 1)), (int(x - 100), int(y + 15)), cv2.FONT_HERSHEY_PLAIN, 2, (100, 200, 0), 2)
-            cv2.imshow("Image", img)
-            key = cv2.waitKey(1)
-            if key == 27:
-                break
-            width_mm, height_mm= format(round(object_width, 1)), (int(x - 100), int(y - 20)), cv2.FONT_HERSHEY_PLAIN, 2, (100, 200, 0), format(round(object_height, 1)), (int(x - 100), int(y + 15)), cv2.FONT_HERSHEY_PLAIN, 2, (100, 200, 0), 2
-        return width_mm*height_mm
+    if webcam:success,img = cap.read()
+    else: img = cv2.imread(path)
+ 
+    imgContours , conts = utlis.getContours(img,minArea=50000,filter=4)
+    if len(conts) != 0:
+        biggest = conts[0][2]
+        #print(biggest)
+        imgWarp = utlis.warpImg(img, biggest, wP,hP)
+        imgContours2, conts2 = utlis.getContours(imgWarp, minArea=2000, filter=4,cThr=[50,50],draw = False)
+        if len(conts) != 0:
+            for obj in conts2:
+                cv2.polylines(imgContours2,[obj[2]],True,(0,255,0),2)
+                nPoints = utlis.reorder(obj[2])
+                nW = round((utlis.findDis(nPoints[0][0]//scale,nPoints[1][0]//scale)/10),1)
+                nH = round((utlis.findDis(nPoints[0][0]//scale,nPoints[2][0]//scale)/10),1)
+                cv2.arrowedLine(imgContours2, (nPoints[0][0][0], nPoints[0][0][1]), (nPoints[1][0][0], nPoints[1][0][1]),(255, 0, 255), 3, 8, 0, 0.05)
+                cv2.arrowedLine(imgContours2, (nPoints[0][0][0], nPoints[0][0][1]), (nPoints[2][0][0], nPoints[2][0][1]),(255, 0, 255), 3, 8, 0, 0.05)
+                x, y, w, h = obj[3]
+                cv2.putText(imgContours2, '{}cm'.format(nW), (x + 30, y - 10), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1.5,(255, 0, 255), 2)
+                cv2.putText(imgContours2, '{}cm'.format(nH), (x - 70, y + h // 2), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1.5,(255, 0, 255), 2)
+        cv2.imshow('A4 Paper', imgContours2)
+    img = cv2.resize(img,(0,0),None,0.5,0.5)
+    cv2.imshow('Original Image',img)
+    cv2.waitKey(1)
 
 # ━━━━━━━━━━━━━━━━━━━━━ Distance function ━━━━━━━━━━━━━━━━━━━━━
-def distance(): # Distance
-    #global path
-    GPIO.output(TRIG_Pin, False)
-    time.sleep(2)
-    GPIO.output(TRIG_Pin, True)
-    time.sleep(0.00001)
-    GPIO.output(TRIG_Pin, False)
-    while GPIO.input(ECHO_Pin)==0:
-        pulse_start = time.time()
-    while GPIO.input(ECHO_Pin)==1:
-        pulse_end = time.time()
-    pulse_duration = pulse_end-pulse_start
-    distance = pulse_duration*17150
-    distance = round(distance,2)
-    return distance
+# def distance(): # Distance
+#     #global path
+#     GPIO.output(TRIG_Pin, False)
+#     time.sleep(2)
+#     GPIO.output(TRIG_Pin, True)
+#     time.sleep(0.00001)
+#     GPIO.output(TRIG_Pin, False)
+#     while GPIO.input(ECHO_Pin)==0:
+#         pulse_start = time.time()
+#     while GPIO.input(ECHO_Pin)==1:
+#         pulse_end = time.time()
+#     pulse_duration = pulse_end-pulse_start
+#     distance = pulse_duration*17150
+#     distance = round(distance,2)
+#     return distance
 
-def volumeMeasure():
-    distanceValue=distance()
-    if(distance<=40):
-        return distance()*areaMeasure
+# def volumeMeasure():
+#     distanceValue=distance()
+#     if(distance<=40):
+#         return distance()*areaMeasure
 
 # ━━━━━━━━━━━━━━━━━━━━━ Setup function ━━━━━━━━━━━━━━━━━━━━━
-def setup(): #Setup
-    global db,company,path,machineID,errorcode
-    global hourNow,tempHour
-    global TRIG_Pin,ECHO_Pin
-    GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BCM)
-    company=path.child("SETUP").child(machineID).get().val()
-    print(company)
-    path=db.child("COMPANY").child(company).child(machineID)
-    path.child("INFO").update({"ERROR_CODE": errorcode,})
-    GPIO.setup(ECHO_Pin, GPIO.IN)
-    GPIO.setup(TRIG_Pin, GPIO.OUT)
-    print("Setup ok")
+# def setup(): #Setup
+#     global db,company,path,machineID,errorcode
+#     global hourNow,tempHour
+#     global TRIG_Pin,ECHO_Pin
+#     GPIO.setwarnings(False)
+#     GPIO.setmode(GPIO.BCM)
+#     company=path.child("SETUP").child(machineID).get().val()
+#     print(company)
+#     path=db.child("COMPANY").child(company).child(machineID)
+#     path.child("INFO").update({"ERROR_CODE": errorcode,})
+#     GPIO.setup(ECHO_Pin, GPIO.IN)
+#     GPIO.setup(TRIG_Pin, GPIO.OUT)
+#     print("Setup ok")
 
 # ━━━━━━━━━━━━━━━━━━━━━ Loop function ━━━━━━━━━━━━━━━━━━━━━
 def loop(): # Loop
-    global db,company,path,machineID,errorcode
-    global hourNow,tempHour
-    global TRIG_Pin,ECHO_Pin
-    hourNow,tempHour= datetime.now().hour, datetime.now().hour # Now time
-    print(hourNow)
+    # global db,company,path,machineID,errorcode
+    # global hourNow,tempHour
+    # global TRIG_Pin,ECHO_Pin
+    # hourNow,tempHour= datetime.now().hour, datetime.now().hour # Now time
+    # print(hourNow)
+    areaMeasure()
 
 # ━━━━━━━━━━━━━━━━━━━━━ Main Code ━━━━━━━━━━━━━━━━━━━━━
 if __name__ == "__main__": # Main funtion
-    setup() # Setup worked
+    # setup() # Setup worked
     while True: # Loop started
         loop()
